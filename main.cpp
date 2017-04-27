c645b9a11bd6d8445adb15894348c0724ea647db
unknown <>
2017-04-27 17:51:04 +0900

    mainwindow flags, attribute set

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::FramelessWindowHint); // hide title bar
+    // w.setAttribute(Qt::WA_TranslucentBackground);    // transparent
+    // w.setMask(); // transparent by pixmap
     w.show();
 
     return a.exec();
